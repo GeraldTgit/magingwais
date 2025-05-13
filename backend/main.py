5898b8f3267243b34d40a2f6733f2ccb8703ff42
GeraldTgit <geralddavetrajano@gmail.com>
2025-05-13 23:34:56 +0800

    Update: Front-end compatibility

--- backend/main.py
+++ backend/main.py
@@ -25,15 +25,18 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://magingwais.vercel.app",
+    "http://magingwais.vercel.app",
+    "https://magingwais-production.up.railway.app",
+    "http://magingwais-production.up.railway.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
+    expose_headers=["Content-Type", "Authorization"],
     max_age=3600,
 )
 
